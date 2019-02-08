e1c0baa332b71158873f717711ee39c49974ab19
Sandy Ynag <yang21@uw.edu>
2019-02-07 16:53:55 -0800

    hi1111

--- analysis.R
+++ analysis.R
@@ -35,9 +35,32 @@ month_plot <- ggplot(data = by_month) +
 # Store plot in a variable
 
 # Map evictions in 2017 
+evictions_2017 <- evictions %>% 
+  mutate(date = as.Date(File.Date, format="%m/%d/%y")) %>% 
+  filter(format(date, "%Y") == "2017") %>%
+  separate(Location, c("lat", "long"), ", ") %>% # split the column at the comma
+  mutate(
+    lat = as.numeric(gsub("\\(", "", lat)), # remove starting parentheses
+    long = as.numeric(gsub("\\)", "", long)) # remove closing parentheses
+  ) 
+
+
 
 # Format the lat/long variables, filter to 2017
+base_plot <- qmplot(
+  data = evictions_2017,        # name of the data frame
+  x = long,                     # data feature for longitude
+  y = lat,                      # data feature for latitude
+  geom = "blank",               # don't display data points (yet)
+  maptype = "toner-background", # map tiles to query
+  darken = .7,                  # darken the map tiles
+  legend = "topleft"            # location of legend on page
+)
 
 # Create a maptile background
+evictions_plot <- base_plot +
+  geom_point(mapping = aes(x = long, y = lat), color = "red", alpha = .3) +
+  labs(title = "Evictions in San Francisco, 2017") +
+  theme(plot.margin = margin(.3, 0, 0, 0, "cm"))
 
 # Add a layer of points on top of the map tiles
